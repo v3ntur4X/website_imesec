24f4175a115574ab0985790c3dfebc9fb9fe2673
Pedro Pereira <pedro.leyton.pereira@usp.br>
2020-04-24 10:22:53 +0100

    Remove old deployment files (#15)

--- interact.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CONTAINER_NAME=nginx-website
-echo Waiting for container $CONTAINER_NAME...
-until [[ $(docker inspect -f {{.State.Running}} $CONTAINER_NAME) == "true" ]]; do
-    sleep 0.5;
-done;
-clear
-docker exec -it $CONTAINER_NAME /bin/sh -c "/bin/bash || /bin/sh"
--- pull.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it nginx-website bash -c "cd /var/www/html && git pull"
